47ac8fa9fd9f59fa467558a7c309b58339fc4e31
UKnow-IKnow <sarojsahu014@gmail.com>
2024-04-04 02:07:02 +0530

    add: create the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,14 @@ import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import About from './pages/About'
 import Projects from './pages/Projects'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+
+      <Header/>
+
       <Routes>
 
         <Route path='/' element= {<Home />} />
@@ -18,6 +22,7 @@ export default function App() {
         <Route path='/dashboard' element= {<Dashboard />} />
         <Route path='/about' element= {<About />} />
         <Route path='/projects' element= {<Projects />} />
+
       </Routes>
     </BrowserRouter>
   )
