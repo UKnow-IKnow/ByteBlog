72376b0a464c46fdf3300ec211566260b3d12ad5
UKnow-IKnow <sarojsahu014@gmail.com>
2024-04-04 12:22:21 +0530

    add: connect to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO)
+    .then(() => { console.log('mongo is connected') }).catch(err => { console.log(err) });
 
 const app = express()
 
-app.listen(3000, () =>{
+app.listen(3000, () => {
     console.log('yes server is working')
 })
